e70530fe3d08dfc838ff8371358017467769cb72
MarkPeng <markpengisme@gmail.com>
2023-02-19 17:41:23 +0800

    go

---
+++ go/stocks/money.go
@@ -0,0 +1,33 @@
+package stocks
+
+type Money struct {
+	amount   float64
+	currency string
+}
+
+func (m Money) Add(other *Money) *Money {
+	var result Money
+	if m.currency == other.currency {
+		result = Money{amount: m.amount + other.amount, currency: m.currency}
+		return &result
+	}
+	return nil
+}
+
+func (m Money) Times(multiplier int) Money {
+	return Money{
+		amount:   m.amount * float64(multiplier),
+		currency: m.currency,
+	}
+}
+
+func (m Money) Divide(divisor int) Money {
+	return Money{
+		amount:   m.amount / float64(divisor),
+		currency: m.currency,
+	}
+}
+
+func NewMoney(amount float64, currency string) Money {
+	return Money{amount, currency}
+}
